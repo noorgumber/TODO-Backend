917b8255f9450794aceb9c70b651c3e9b0c846fa
NOOR GUMBER <noor.gumber23@gmail.com>
2025-12-02 20:11:07 +0530

    routes

--- server.js
+++ server.js
@@ -1,26 +1,40 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
-const app=express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=> console.log("DB connected"))
-.catch(()=> console.log(err))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(() => console.log(err));
 
-const Todo = mongoose.model("Todo", new mongoose.schema({
-  text:String
-}));
+const Todo = mongoose.model(
+  "Todo",
+  new mongoose.Schema({
+    text: String,
+  })
+);
 
-app.get("/todos", async(req, res)=> {
+app.get("/todos", async (req, res) => {
   res.json(await Todo.find());
+});
+
+app.post('/todos', async(req,res) => {
+  const todo=await Todo.create({ text:req.body.text});
+  res.json(todo);
 })
 
-app.listen(process.env.PORT, ()=> 
+app.delete("/todo/:id", async(req,res)=>{
+  await Todo.findByIdAndDelete(req.paramas.id);
+  res.json({message : "Deleted"})
+})
+
+app.listen(process.env.PORT, () =>
   console.log("Server running on port", process.env.PORT)
-)
+);
